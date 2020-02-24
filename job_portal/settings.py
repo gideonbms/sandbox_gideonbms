6ea8723133987202d0cb41949ac0e974a6625f19
gideonbms <gideonbms@yahoo.com>
2020-02-24 01:18:35 +0000

    Another update

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -137,11 +137,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'job_portal/static'),
-]
-
 STATIC_ROOT = 'home/algo/job_portal/static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
